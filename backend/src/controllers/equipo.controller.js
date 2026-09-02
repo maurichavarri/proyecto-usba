b74ba131205a82a2139cad6ec5e5f06eb374f7c2
CelesteCaelum777 <priscilasinger04@gmail.com>
2026-09-02 10:53:41 -0300

    Merging2

--- backend/src/controllers/equipo.controller.js
+++ backend/src/controllers/equipo.controller.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// src/controllers/equipo.controller.js
-=======
->>>>>>> f9795a5b6e129b64176c2a4300c271c304d9b0f0
 import { Sequelize } from 'sequelize';
 import Equipo from '../models/equipo.model.js';
 import Jugador from '../models/jugador.model.js';
@@ -17,10 +13,6 @@ export const crearEquipo = async (req, res, next) => {
             creado_en: new Date().getFullYear(),
             id_usuario_creador: usuarioId
         });
-<<<<<<< HEAD
-        
-=======
->>>>>>> f9795a5b6e129b64176c2a4300c271c304d9b0f0
         res.status(201).json(equipo);
     } catch (error) {
         console.error('Error al crear equipo:', error);
@@ -39,27 +31,16 @@ export const obtenerMisEquipos = async (req, res, next) => {
                 include: [
                     [
                         Sequelize.literal(`
-<<<<<<< HEAD
-                            (SELECT COUNT(*) FROM jugador WHERE jugador.equipo_id = Equipo.id)
-                        `),
-=======
                 (
                     SELECT COUNT(*)
                     FROM jugador
                     WHERE jugador.equipo_id = Equipo.id
                 )
             `),
->>>>>>> f9795a5b6e129b64176c2a4300c271c304d9b0f0
                         'cantidad_jugadores'
                     ],
                     [
                         Sequelize.literal(`
-<<<<<<< HEAD
-                            (SELECT COUNT(*) FROM inscripcion 
-                             WHERE inscripcion.equipo_id = Equipo.id 
-                             AND inscripcion.estado = 'confirmado')
-                        `),
-=======
                 (
                     SELECT COUNT(*)
                     FROM inscripcion
@@ -67,7 +48,6 @@ export const obtenerMisEquipos = async (req, res, next) => {
                     AND inscripcion.estado = 'confirmado'
                 )
             `),
->>>>>>> f9795a5b6e129b64176c2a4300c271c304d9b0f0
                         'cantidad_competencias'
                     ]
                 ]
